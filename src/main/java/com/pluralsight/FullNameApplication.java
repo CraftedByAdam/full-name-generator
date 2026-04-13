e687bc20d8fcde45a421fb5071d609b53ca7ebe7
Your Name <youremail@example.com>
2026-04-13 13:50:14 -0400

    Made a scanner but cant figure out what to do next without looking up the way on the internet.

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -1,4 +1,28 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FullNameApplication {
+    static void main() {
+        System.out.print("What is your name? Press Enter to Start");
+        Scanner scanner = new Scanner(System.in);
+
+
+        String firstName = scanner.nextLine();
+        System.out.println("First Name: ");
+
+        String middleInitial = scanner.next();
+        System.out.println("Middle Initial: ");
+
+        String lastName = scanner.nextLine();
+        System.out.println("Last Name: ");
+        scanner.nextLine();
+
+        System.out.println(firstName + " " + middleInitial + ", " + lastName);
+        scanner.close();
+        String fullName = "";
+
+
+
+    }
 }
